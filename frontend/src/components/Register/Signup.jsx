930b594abca9986d8e691616558dca6b678cbd13
Ritiksharma0004 <142799642+Ritiksharma0004@users.noreply.github.com>
2024-12-09 23:32:11 +0530

    Update Signup.jsx

--- frontend/src/components/Register/Signup.jsx
+++ frontend/src/components/Register/Signup.jsx
@@ -58,9 +58,7 @@ function Signup() {
   };
 
 
-res.setHeader('Access-Control-Allow-Origin', '*');
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
 
 app.options('*', cors());
 
